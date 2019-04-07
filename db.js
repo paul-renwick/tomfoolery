3a4ea6d51906d955096385d2945e6343eab19ca9
paul-renwick <46697581+paul-renwick@users.noreply.github.com>
2019-04-07 12:03:22 +1200

    Add files via upload
    Starting with existing boilerplate

---
+++ db.js
@@ -0,0 +1,16 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+  getUser: getUser,
+  getUsers: getUsers
+}
+
+function getUsers (db = connection) {
+  return db('users').select()
+}
+
+function getUser (id, db = connection) {
+  return db('users').where('id', id).first()
+}
---
+++ tests/db.test.js
@@ -0,0 +1,32 @@
+const testEnv = require('./test-environment')
+const db = require('../db')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = testEnv.getTestDb()
+  return testEnv.initialise(testDb)
+})
+
+afterEach(() => testEnv.cleanup(testDb))
+
+test('getUsers gets all users', () => {
+  // One for each letter of the alphabet!
+  const expected = 3
+  return db.getUsers(testDb)
+    .then(users => {
+      const actual = users.length
+      expect(actual).toBe(expected)
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+test('getUser gets a single user', () => {
+  const expected = 'test user 1'
+  return db.getUser(99901, testDb)
+    .then(user => {
+      const actual = user.name
+      expect(actual).toBe(expected)
+    })
+    .catch(err => expect(err).toBeNull())
+})
